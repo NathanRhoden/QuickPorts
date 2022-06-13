dc4bfe0dab942846b37b853e9735d4a25fc66a1d
Nathan <Nathan.rhoden@gmail.com>
2022-06-13 20:56:35 +0100

    initial commit to setup backend

---
+++ src/main/java/com/example/nr/quickports/QuickportsApplication.java
@@ -0,0 +1,13 @@
+package com.example.nr.quickports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nr/quickports/QuickportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nr.quickports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
