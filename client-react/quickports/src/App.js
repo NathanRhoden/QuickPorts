ea8e8b4873248643241ff578b7c3d397e081d14b
Nathan <Nathan.rhoden@gmail.com>
2022-07-04 22:54:52 +0100

    Created googleMap container component and added Search bar column component -Search Bar component will display information regading the charge points. -TODO : Correct the name of the Column class as the component will not have search functionality.

--- client-react/quickports/src/App.js
+++ client-react/quickports/src/App.js
@@ -1,19 +1,22 @@
-import './App.css';
-import GoogleMapContainer from './components/map/GoogleMapContainer';
-
+import "./App.css";
+import GoogleMapContainer from "./components/map/GoogleMapContainer";
+import SearchColumn from "./components/searchformbar/SearchColumn";
+import "./components/map/GoogleMapContainer.css";
 
 function App() {
-
   const mapConfig = {
-
-    zoom: 13 , 
-    center: { lat: 51.507351, lng: -0.127758 }
-  }
-
+    zoom: 13,
+    center: { lat: 51.507351, lng: -0.127758 },
+  };
 
   return (
     <div className="App">
-        <GoogleMapContainer zoom={mapConfig.zoom} center={mapConfig.center}/>
+      <div className="[map-container]">
+        <GoogleMapContainer zoom={mapConfig.zoom} center={mapConfig.center} />
+      </div>
+      <div>
+        <SearchColumn />
+      </div>
     </div>
   );
 }
